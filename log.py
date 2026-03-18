e745a8cd17cd9a64cb5ed006b58d961087a735bb
HinaTrue <hina@truefoundry.com>
2026-03-18 17:49:47 +0530

    Update log.py

--- log.py
+++ log.py
@@ -6,5 +6,5 @@ duration = 5 * 60  # 5 minutes in seconds
 
 while time.time() - start_time < duration:
     current_time = datetime.now().strftime("%H:%M:%S")
-    print(current_time)
-    time.sleep(1)  # wait 1 second
+    print(current_time, flush=True)
+    time.sleep(1)
